5514bcd590bfb30a4a788c7d015656bbf216cf41
LuisArroyoCaballero <alum.larroyoc.2020@iesalixar.org>
2020-10-18 19:56:17 +0200

    Terminado hasta EJ4-5

--- Java/src/main/java/U2/T4/Ejercicio4_5.java
+++ Java/src/main/java/U2/T4/Ejercicio4_5.java
@@ -1,14 +1,15 @@
 package U2.T4;
 //Actividad 4.5 - Escribe un programa que muestre la hora actual
 
-import java.util.SimpleTimeZone;
+import java.time.LocalDateTime;
 
 public class Ejercicio4_5 {
     public static void main(String arg[]) {
-        SimpleTimeZone reloj = new SimpleTimeZone();
-        int hora = reloj.nextInt(
-        System.out.println(hora);
+        LocalDateTime locaDate = LocalDateTime.now();
+        int hours = locaDate.getHour();
+        int minutes = locaDate.getMinute();
+        int seconds = locaDate.getSecond();
+        System.out.println("Hora actual : " + hours + ":" + minutes + ":" + seconds);
 
     }
 }
-
